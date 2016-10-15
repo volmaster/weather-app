128644fbc2ffbe86eb9b690a18781ac69bf60ad2
volmaster <volmasters@gmail.com>
2016-10-15 10:22:29 +0300

    code edits

--- main.py
+++ main.py
@@ -2,25 +2,14 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+app.secret_key = "\xbeE\xd6X\xc7\x9c\x85\x03\xad\x07\xed\xfa\xc6\xa1\xe1f\xf8\x0bn^\xce\x94\xff:"
 
 
-def city_id(id_str):
-    param = {'id': id_str, 'units': 'metric', 'APPID': '64ae36ec03b368c4e5b0754df2b7e733'}
+def weather_data(val):
+    param = {'q': val, 'units': 'metric',
+             'APPID': '64ae36ec03b368c4e5b0754df2b7e733'}
     url = 'http://api.openweathermap.org/data/2.5/weather?'
-    req = requests.get(url, params=param)
-    return req
-
-
-# TODO add form when user can input their city
-
-req_lv = city_id('702550')
-req_new = city_id('5128581')
-req_ry = city_id('108410')
-req_to = city_id('1850147')
-
-
-def weather_data(req):
-    json_data = req.json()
+    json_data = requests.get(url, params=param).json()
     city = json_data['name']
     temp = int(json_data['main']['temp'])
     weather = json_data['weather'][0]['description']
@@ -28,15 +17,14 @@ def weather_data(req):
     return data_dict
 
 
-info_lv = weather_data(req_lv)
-info_new = weather_data(req_new)
-info_ry = weather_data(req_ry)
-info_to = weather_data(req_to)
+city_list = ['Lviv', 'New York', 'Riyadh', 'Tokyo']
+
+info = list(map(weather_data, city_list))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', info_lv=info_lv, info_new=info_new, info_ry=info_ry, info_to=info_to)
+    return render_template('index.html', info=info)
 
 
 if __name__ == '__main__':
